e8547c20f6f382c81588cf8e0fd4453e0a1e1e1c
Chakradhar Rao <chakradhar.jonagam@biqmind.com>
2020-11-19 11:54:22 +0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,6 @@ master="master"
 if [ "${GITHUB_REF##*/}" == "$master" ]; then
     echo 'RELEASE_TAG=latest' >> $GITHUB_ENV
 else
-    tag="${GITHUB_REF##*/}"
-    echo $tag
-    echo 'RELEASE_TAG=${tag}' >> $GITHUB_ENV
+    tag="RELEASE_TAG="${GITHUB_REF##*/}
+    echo $tag >> $GITHUB_ENV
 fi
