e928f9314fd79a4448c2affdeee853929fa9b0a6
Chakradhar Rao Jonagam <debianmaster@users.noreply.github.com>
2020-11-19 12:02:46 +0800

    Fix/set env depricate (#1)
    fixes set-env github deprication

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -l
-echo ::set-env name=RELEASE_TAG::$(echo ${GITHUB_REF##*/})
 master="master"
 if [ "${GITHUB_REF##*/}" == "$master" ]; then
-    echo ::set-env name=RELEASE_TAG::latest
+    echo 'RELEASE_TAG=latest' >> $GITHUB_ENV
+else
+    tag="RELEASE_TAG="${GITHUB_REF##*/}
+    echo $tag >> $GITHUB_ENV
 fi
