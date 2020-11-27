378b405bcda62edb4c16c00fcf0d7d1feb7abc33
Chakradhar Rao <chakradhar.jonagam@biqmind.com>
2020-11-27 13:24:58 +0800

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/sh -l
 master="master"
-printenv
-cat /github/workflow/event.json
 if [ "${GITHUB_REF##*/}" == "$master" ]; then
     echo 'RELEASE_TAG=latest' >> $GITHUB_ENV
+elif [ $(cat /tmp/event.json | jq -r .action) == "release-to-env" ]
+then
+    tag=$(cat /tmp/event.json  | jq -r .client_payload.tag)
+    echo $tag >> $GITHUB_ENV
 else
     tag="RELEASE_TAG="${GITHUB_REF##*/}
     echo $tag >> $GITHUB_ENV
